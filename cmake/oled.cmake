e18670e2a6f2ac98ef3b83aa358e9e56cd8b994e
John Wass <wassj@ctc.com>
2020-11-25 21:01:00 -0500

    pure conan

--- cmake/oled.cmake
+++
@@ -1,8 +0,0 @@
-set(oled ${CONAN_FEATHER-OLED_ROOT})
-
-file(GLOB SOURCE_FILES ${oled}/src/*)
-
-add_library(oled STATIC ${SOURCE_FILES})
-target_include_directories(oled PRIVATE ${oled}/include ${PLATFORM_CXX_INCLUDES})
-target_compile_options(oled PRIVATE "$<$<CONFIG:ALL>:${PLATFORM_CXX_FLAGS}>")
-target_compile_definitions(oled PRIVATE ${PLATFORM_CXX_DEFS})
